65dc92efdf6a87d0f4e7bdaed1ac14ccbf9e9c53
AnwarProgramming <Anwar.iitmbs@gmail.com>
2024-12-24 19:57:40 +0530

    askhdfa

--- 1.Basic.py
+++ 1.Basic.py
@@ -1,12 +1,18 @@
-##########  1/95 #############
-########## 2/95 ############
+######################  1/95 ##########################
+######################  2/95 ##########################
 
 print("Hello World !")
+print("*")
+print("**")
+print("***")
+
+print("I don't know what to do !")
 
 
+###################### 3/95 ##########################
 
+print("Hello World!", 'Hello India!') # single quatation ('') or double quatation ("") must be used to write string
+print(10) # In the case of number no quatation is needed
 
-
-
-
+print("Hello India", 10, 10.5)
 
---
+++ 2.Variables.py
@@ -0,0 +1,44 @@
+######################  4/95 ##########################
+
+# a = 10
+# print(a)
+
+# b = 20
+# print(b)
+
+# print(a+b)
+# print(a*b)
+# print(b/a)
+
+###
+
+# c = 10
+# print(c)
+# c = c+1
+# print(c)
+# c = c+1
+# print(c)
+# c = c+1
+# print(c)
+
+###
+
+# print("Enter a number:")
+# n = int(input())
+# n = n+1
+# print(n)
+# print(n+1)
+# print(n+2)
+# print(n+3)
+
+######################  5/95 ##########################
+### Variables and Input Statement
+print("Hello, type in your name:")
+n = str(input())
+print("Hello,",n,".")
+print("Which place are you in ?")
+p = str(input())
+print("Hello,",n,".","How is the weather in",p,"?")
+print("What is your age ?")
+age = int(input())
+print("Good to know your are",age, "years old.")
