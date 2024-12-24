ae7dedccedc61c52b2c3eca764cdd1060ea7492b
AnwarProgramming <Anwar.iitmbs@gmail.com>
2024-12-24 20:50:14 +0530

    variables added

--- 2.Variables.py
+++ 2.Variables.py
@@ -33,12 +33,31 @@
 
 ######################  5/95 ##########################
 ### Variables and Input Statement
-print("Hello, type in your name:")
-n = str(input())
-print("Hello,",n,".")
-print("Which place are you in ?")
-p = str(input())
-print("Hello,",n,".","How is the weather in",p,"?")
-print("What is your age ?")
-age = int(input())
-print("Good to know your are",age, "years old.")
+
+# print("Hello, type in your name:")
+# n = str(input())
+# print("Hello,",n,".")
+# print("Which place are you in ?")
+# p = str(input())
+# print("Hello,",n,".","How is the weather in",p,"?")
+# print("What is your age ?")
+# age = int(input())
+# print("Good to know your are",age, "years old.")
+
+
+######################  6/95 ##########################
+### Variables and Literals
+
+# n = str(input("Type your name:"))
+# p = str(input("Type your location:"))
+# print("Hello",n,"."" How is the weather in",p,"?")
+# age = int(input("What is your age ?"))
+# print("Good to know you are",age,"years old.")
+
+###
+age = 30 # Here age is a variable and 30 is literals
+
+###
+r = int(input("Enter the radius of the circle:"))
+area = 3.14*r*r
+print("Area of the circle with radius",r,"is",area)
