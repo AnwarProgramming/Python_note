0b5ca57085776a286436e69d6cd436a1e30bd750
AnwarProgramming <Anwar.iitmbs@gmail.com>
2024-12-25 10:04:16 +0530

    data type added

---
+++ 3.Data_Type.py
@@ -0,0 +1,68 @@
+######################  7/95 ##########################
+### Data Type 1
+
+# n = 10
+# print(n)
+# r = 6.3
+# print(r)
+# s = "India"
+# print(s)
+
+# print("n is of type:",type(n))
+# print("r is of type:",type(r))
+# print("s is of type:",type(s))
+
+###
+# l = [10,20,30,68,732]
+
+# print(l[0])
+# print(l[1])
+# print(l[2])
+# print(l[3])
+# print(l[4])
+
+# print("l is of type:",type(l))
+# print(type(l[2]))
+
+######################  8/95 ##########################
+### Data Type 2
+
+# i = 10
+# f = 5.6
+# s = "India"
+# b1 = True
+# b2 = False
+
+# print(type(i))
+# print(type(f))
+# print(type(s))
+# print(type(b1))
+# print(type(b2))
+
+### Type conversion/ Type casting
+# a = int(5.7)
+# b = int("10")
+# print(a,type(a))
+# print(b,type(b))
+
+# a = float(9)
+# b = float('5.3')
+# print(a,type(a))
+# print(b,type(b))
+
+# a = str(9)
+# b = str('5.3')
+# print(a,type(a))
+# print(b,type(b))
+
+a = bool(10.5) # in case of boolian only 0(zero) and empty string("") is false else all values are true
+b = bool(0)  
+c = bool(-10)
+d = bool("string")
+e = bool("")
+
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+print(e,type(e))
