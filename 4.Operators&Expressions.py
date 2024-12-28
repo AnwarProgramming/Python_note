27ef86957c4ef9933b51c38f11b59260cdfc192f
AnwarProgramming <Anwar.iitmbs@gmail.com>
2024-12-28 08:44:21 +0530

    Operators added

--- 4.Operators&Expressions.py
+++ 4.Operators&Expressions.py
@@ -1,3 +1,73 @@
 
 ######################  9/95 ##########################
+### Operators and Expressions
+# a = 'India'
+# b = 'Bharat'
+# n = a+b  # Adding two string is called concatenation
+# print(n)
 
+# a = [1,2,3]
+# b = [2,7,9,15]
+# n = a+b
+# print(n)
+
+# a = 11
+# b = 15
+# n = a/b  # output will be float number
+# print(n)
+
+# n = 10+13*2 # Operator precedence is rules that decide the order in which parts of an expression are calculated
+# print(n)
+
+# n = ((10+13)*2) # to avoid operator precedence just use bracket
+# print(n)
+
+
+######################  10/95 ##########################
+### In python Operators are divided into three major categories - 1.Arithmetics operators, 2.Relational Operators, 3.Logical Operators
+## Arithmetics operators( +, -, *, /, //, %, **)
+
+# print("Addition", 2+3)
+# print("Substraction", 9-1)
+# print("Multiplication", 5*4)
+# print("Division", 7/3)
+
+# print("Floor division", 7// 3) # Floor division is a division operation that returns the largest integer that is less than or equal to the result of the division.
+
+# print("Modulus", 7 % 3) # Modulus operator provides remainder as output
+
+# print("Exponential", 6 ** 2)
+
+# ## Relational Operators(>, <, >=, <=, ==, !=)
+# ## Relational Operators provides only in boolian value.
+# print(5 > 10)
+# print(10 > 5)
+
+# print(5 < 10)
+# print(10 < 5)
+
+# print( 5 > 5)
+# print( 5 >= 5)
+
+# print( 5 < 5)
+# print( 5 <= 5)
+
+# print( 5 == 50)
+# print( 5 == 5)
+
+# print( 5 != 50)
+# print( 5 != 5)
+
+## Logical Operators(and, or, not)
+# print(True and True) # 'and' operator will provide True when all are True
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# print(True or True) # 'or' operator will provide False when all are False
+# print(True or False)
+# print(False or True)
+# print(False or False)
+
+print( not (True)) # not False is True and not True is False
+print( not False )
